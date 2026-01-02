9865b8ff2b9d00369be1f7966df0cefd8c1a1064
Jdestrada <jeremydanielestrada2001@gmail.com>
2026-01-02 23:10:37 +0800

    feat: added auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+//Authentication Routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+
+
+Route::get('/logout', [AuthController::class, 'logout']);
+});
